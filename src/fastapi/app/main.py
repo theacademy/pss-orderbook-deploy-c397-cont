5499024a243c61fd455581ad49cb557023d4e94d
Thea Gregory <thea.isis@gmail.com>
2023-09-21 15:36:52 -0400

    added active users feature

--- src/fastapi/app/main.py
+++ src/fastapi/app/main.py
@@ -222,3 +222,20 @@ async def read_tradeable(
 
     return [ {"name": symbol["symbol"], "price": symbol['price'] } for symbol in symbols ]
 
+@app.post("/active_users")
+async def do_active_user(
+    response: Response,
+    session: UserSession   
+) :
+    
+    if not authorize(session.uname, session.sessionid, ["admin"]): return [{"msg":"not authorized"}]
+
+    users = []
+    keys = cache.keys() # get all keys
+    for k in keys:
+        k = k.decode('utf-8') # make bytes into string
+        # the keys with usernames look like "admin-sessionid"
+        if "-sessionid" in k: #
+            users.append(k.replace("-sessionid", "")) # append only the username
+
+    return { "active-users" : users }
