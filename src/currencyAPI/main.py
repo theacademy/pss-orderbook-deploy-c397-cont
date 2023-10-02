7a16f9ce0ee3eb9a19f3448e986f66419b71a141
XavierSHiga <xavierhiga@gmail.com>
2023-10-02 17:00:22 -0400

    Trying to trigger a branch push action.

--- src/currencyAPI/main.py
+++ src/currencyAPI/main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 
 API_BASE_URL = "https://api.exchangerate-api.com/v4/latest/"
 
+# This is a random comment.
 
 async def get_exchange_rate(from_currency: str, to_currency: str) -> float:
     response = requests.get(f"{API_BASE_URL}{from_currency.upper()}") 
