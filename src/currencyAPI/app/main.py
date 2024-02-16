5b3580bc0683be44d44d4fb2f50687b99712dd07
Alexander Naskinov <alexnaskinov@gmail.com>
2024-02-16 16:15:12 +0000

    updated main

--- src/currencyAPI/app/main.py
+++ src/currencyAPI/app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, HTTPException
 import requests
-
+ 
 app = FastAPI()
-
+ 
 API_BASE_URL = "https://api.exchangerate-api.com/v4/latest/"
-
+COINBASE_API_URL = "https://api.coinbase.com/v2/currencies/crypto"
+COINBASE_RATES = "https://api.coinbase.com/v2/exchange-rates/"
+ 
+ 
 
 async def get_exchange_rate(from_currency: str, to_currency: str) -> float:
-    response = requests.get(f"{API_BASE_URL}{from_currency.upper()}") 
+    response = requests.get(f"{API_BASE_URL}{from_currency.upper()}")
     if response.status_code == 200:
         data = response.json()
         if to_currency.upper() in data["rates"]:
@@ -16,8 +19,19 @@ async def get_exchange_rate(from_currency: str, to_currency: str) -> float:
             raise HTTPException(status_code=400, detail="To currency not supported")
     else:
         raise HTTPException(status_code=400, detail="From currency not supported")
-
-
+ 
+@app.get("/check_password_strength")
+async def check_password_strength(password: str) -> dict:
+    p_uppercase = any(char.isupper() for char in password)
+    p_lowercase = any(char.islower() for char in password)
+    p_digit = any(char.isdigit() for char in password)
+    plength = len(password) >= 8
+ 
+    return {
+        "User_has_strong_password": p_uppercase and p_lowercase and p_digit and plength
+    }
+    # Coded by  Philip Hushani
+ 
 @app.get("/exchange_rate")
 async def exchange_rate(from_currency: str, to_currency: str) -> dict:
     rate = await get_exchange_rate(from_currency, to_currency)
@@ -26,8 +40,8 @@ async def exchange_rate(from_currency: str, to_currency: str) -> dict:
         "to_currency": to_currency.upper(),
         "exchange_rate": rate,
     }
-
-
+ 
+ 
 @app.get("/convert_amount")
 async def convert_amount(from_currency: str, to_currency: str, amount: float) -> dict:
     rate = await get_exchange_rate(from_currency, to_currency)
@@ -38,113 +52,61 @@ async def convert_amount(from_currency: str, to_currency: str, amount: float) ->
         "amount": amount,
         "converted_amount": converted_amount,
     }
-  
-# @CODE : AN ENDPOINT THAT TAKES A STRING AND CONFIRMS IT HAS
-# AT LEAST ONE UPPERCASE LETTER, ONE LOWERCASE LETTER, ONE NUMBER, AND IS 8 OR MORE CHARACTERS
-# Make sure the return type matches the function signature, FastAPI enforces that it does!
-#@app.get("/check_password_strength")
-#async def check_password_strength(password: str) -> bool:
-#    """
-#    Coded By: <name>  
-#    This function checks whether a given password is strong enough, i.e., it contains at least one digit, 
-#    one lowercase letter, one uppercase letter, and is 8 characters long.
-#    """
+ 
+@app.get("/available_currencies")
+async def available_currencies() -> dict:
+    try:
+        response = requests.get(API_BASE_URL)
+        if response.status_code == 200:
+            data = response.json()
+            available_currencies = [currency["id"] for currency in data["data"]]
+            return {"available_currencies": available_currencies}
+        else:
+            raise HTTPException(status_code=400, detail="From currency not supported")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+ 
+#Coded by: Alexander Naskinov
 
-
-# @CODE : ADD ENDPOINT TO LIST ALL AVAILABLE CURRENCIES  
-# NOTE : FastAPI enforces that the return type of the function matches the function signature!  
-#        This is a common error!
-#@app.get("/available_currencies")
-#async def available_currencies(from_currency: str) -> dict:
-#    """
-#    Coded by: <name>  
-#    This endpoint returns a list of available fiat currencies that can be paired with the @from_currency parameter.  
-#    @from_currency : str - you must specify a currency to see what currencies it can be compared against.
-#    """
-
-
-# @CODE : ADD ENDPOINT TO GET LIST OF CRYPTO CURRENCIES
-# You can use this API https://docs.cloud.coinbase.com/sign-in-with-coinbase/docs/api-currencies
-# Search for the endpoint that returns all the crypto currencies.
-#@app.get("/available_crypto")
-#async def available_crypto() -> dict:
-#    """
-#    Coded by: <name>  
-#    This endpoint allows you to see what crypto-currencies are available  
-#    """
-
+@app.get("/available_crypto")
+async def available_crypto() -> dict:
     
-# @CODE : ADD ENDPOINT TO GET Price of crypto  
-# Use the coinbase API from above
-# @app.get("/convert_crypto")
-# async def convert_crypto(from_crypto: str, to_currency: str) -> dict:
+    #Coded by: Bette Beament
+    #This endpoint allows you to see what crypto-currencies are available
+    
+    try:
+        response = requests.get(COINBASE_API_URL)
+        if response.status_code == 200:
+            data = response.json()
+            crypto_currencies = [currency["code"] for currency in data["data"]]
+            return {"crypto_currencies": crypto_currencies}
+        else:
+            raise HTTPException(status_code=400, detail="Failed to fetch crypto currencies")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+ 
+async def get_crypto_rate(crypto_currency: str, fiat_currency: str) -> float:
+    response = requests.get(f"{COINBASE_RATES}{crypto_currency}-{fiat_currency}")
+    if response.status_code == 200:
+        data = response.json()
+        return float(data["data"]["rates"]["value"])
+    else:
+        raise HTTPException(status_code=400, detail="Failed to fetch crypto rate")
+   
+@app.get("/convert_crypto")
+async def convert_crypto(from_crypto: str, to_currency: str, amount: float) -> dict:
 #    """
-#    Coded by: <name>  
+#    Coded by: Tomasz Wisniewski
 #    This endpoint allows you to get a quote for a crypto in any supported currency  
 #    @from_crypto - chose a crypto currency (eg. BTC, or ETH)  
 #    @to_currency - chose a currency to obtain the price in (eg. USD, or CAD)  
 #    """
-
-
-# @CODE : ADD ENDPOINT TO UPDATE PRICE OF ASSET IN ORDERBOOK DB
-# The code below starts you off using SQLAlchemy ORM
-# Dependencies should already be installed from your requirements.txt file
-# Using the ORM instead of raw SQL is safer and less coupled: it is best practice!
-@app.get("/update_orderbookdb_asset_price")
-async def update_orderbookdb_asset_price(symbol: str, new_price: float) -> dict:
-    """
-    Coded by: <name>  
-    This endpoint allows us to update the price of the assets in the app  
-    @symbol - pick a symbol to update the price of in the orderbook app  
-    @new_price - The new price of the symbol  
-    """
-    
-    # import sqlalchemy
-    from sqlalchemy import create_engine, Table, Column, String, DateTime, Numeric, update, MetaData
-    from sqlalchemy.orm import sessionmaker
-    
-    # create an engine for building sessions
-    engine = create_engine('mysql+pymysql://wiley:wiley123@orderbookdb/orderbook')
-
-    # create an ORM object that maps to the Product table
-    metadata = MetaData()
-    product_table = Table('Product', metadata,
-        Column('symbol', String(16), primary_key=True),
-        Column('price', Numeric(precision=15, scale=2)),
-        Column('productType', String(12)),
-        Column('name', String(128)),
-        Column('lastUpdate', DateTime)
-    )
-    metadata.create_all(engine)
-
-    # create a database session maker
-    Session = sessionmaker(bind=engine)
-
-    try:
-        # Instantiate the session
-        session = Session()
-        # create the statement to udpate
-        stmt = update(product_table).where(product_table.c.symbol == symbol).values(price=new_price)
-        # execute commit and flush the statement
-        session.execute(stmt)
-        session.commit()
-        session.flush()
-        return {"update_report": "success", "symbol": symbol, "new_price": new_price}
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=400, detail="An error occurred, make sure symbol exists and price is numeric")
-    finally:
-        session.close()
-
-    
-# @CODE : ADD ENDPOINT FOR INSERTING A CRYPTO CURRENCY INTO THE ORDERBOOK APP
-# HINT: Make use of the convert_crypto function from above! 
-#       You will need to use the await keyword to wait for the result (otherwise it will run async and not wait for the result)
-#@app.get("/add_crypto_to_orderbook")
-#async def add_crypto_to_orderbook(symbol: str) -> dict:
-#    """
-#     Coded by: <name>  
-#     This endpoint uses the `convert_crypto` function above to get the price of a crypto-currency  
-#     and inserts that currency and price into the orderbook database 
-#    """
-  
+    crypto_rate = await get_crypto_rate(from_crypto, to_currency)
+    converted_amount = amount * crypto_rate
+    return {
+        "crypto_currency": from_crypto.upper(),
+        "fiat_currency": to_currency.upper(),
+        "crypto_rate": crypto_rate,
+        "amount": amount,
+        "converted_amount": converted_amount,
+    }
