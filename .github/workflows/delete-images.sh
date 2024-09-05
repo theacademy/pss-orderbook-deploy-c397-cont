290757e5893f197e9f0e5c69187556789b957c4b
Emily Benoit <82483267+itisnotemily@users.noreply.github.com>
2024-09-05 11:47:09 -0400

    Create delete-images.sh
    no actual code yet - moving the bash script for deleting here, to call from yml file of same name

---
+++ .github/workflows/delete-images.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#seperating out the bash command for the delete-images workflow into its own shell script file 
+#so that it runs all at once and not line by line? perhaps 
+
+#...we'll see if this actually works 
